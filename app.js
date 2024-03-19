8e507580ebdcd49d93cbbe84fda4825832aaa6c3
omaralexistorbiscopizarro <omar16354@gmail.com>
2024-03-18 22:50:51 -0500

    Utilizando JavaScript

---
+++ app.js
@@ -0,0 +1,26 @@
+let usuarioNombre = promt("¿Cual es tu nombre?");
+console.log("Bienvenido " + usuarioNombre);
+document.write("Bienvenido " + usuarioNombre);
+
+let usuarioEdad = promt("¿Cual es tu edad?");
+console.log("Edad= " + usuarioEdad);
+document.write("Edad= " + usuarioEdad);
+
+let cotizacion = confirm("¿quisieras solicitar una cotizacion?");
+if(cotizacion = true){
+    console.log("Eres un Cliente" + usuarioNombre);
+    document.write("Perfecto " + usuarioNombre +"!, en breve te contactaremos ;)");
+}else{
+    console.log("Eres un Visitante" + usuarioNombre);
+    document.write("No hay problema " + usuarioNombre +" , te invitamos a revisar nuestros servicios ;)");
+}
+
+let modoOscuro = confirm("¿Quieres cambiar a modo oscuro?");
+if(modoOscuro = true){
+    document.body.style.backgroundColor = "black";
+    console.log("Activa modo oscuro" + usuarioNombre);
+    document.write("Perfecto " + usuarioNombre +"!, ahora estas ahorrando energia ;)");
+}else{
+    console.log("No activa modo oscuro" + usuarioNombre);
+    document.write("Perfecto " + usuarioNombre +"!, eres el jefe! ;)");
+}
